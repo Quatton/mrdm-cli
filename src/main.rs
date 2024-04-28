560908de0b8ca34b5dd5d120f27ef89eb186e381
Quatton <atomicatives@gmail.com>
2024-04-28 16:06:39 +0900

    feat: data.json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use anyhow::{Context, Result};
-use clap::{command, Args, Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use config::Config;
 
+use log::debug;
 use regex::Regex;
-use serde_derive::{Deserialize, Serialize};
-use std::io::{BufWriter, Write};
+use serde::{Deserialize, Serialize};
+use std::{
+    io::{BufWriter, Write},
+    sync::{Arc, Mutex},
+    thread,
+};
 #[derive(Debug, Parser)] // requires `derive` feature
 #[command(name = "mrdm")]
 #[command(about = "A //TODO list utility for in-code project management", long_about = None)]
@@ -18,9 +23,9 @@ enum Commands {
     /// Manage TODOs in a file
     Todo(TodoArgs),
 
-    // TODO: `mrdm commit` collect TODOs into a change file and add an idempotency key so that you can move and rename
+    // TODO(0): `mrdm commit` collect TODOs into a change file and add an idempotency key so that you can move and rename
 
-    // TODO: `mrdm commit` should help with committing with name and description
+    // TODO(1): `mrdm commit` should help with committing with name and description
     Init,
 }
 
@@ -34,7 +39,7 @@ struct TodoArgs {
 enum TodoCommands {
     /// List TODOs in a file
     List {
-        // TODO: pattern should accept more tags like feat, fix, case-insensitive -> config file
+        // TODO(2): pattern should accept more tags like feat, fix, case-insensitive -> config file
         /// A comma separated pattern to search for in the TODOs
         /// example: "TODO,HACK,FIXME"
         #[arg(short)]
@@ -67,6 +72,7 @@ impl ::std::default::Default for CliConfig {
     }
 }
 
+#[derive(Debug, Deserialize, Serialize)]
 struct TodoItem {
     title: String,
     category: String,
@@ -74,7 +80,13 @@ struct TodoItem {
     line: usize,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct TodoList {
+    items: std::collections::HashMap<String, TodoItem>,
+}
+
 const CONFIG_PATH: &str = "mrdm.json";
+const OUT_PATH: &str = ".mrdm/data.json";
 
 fn get_config() -> CliConfig {
     // this will never error, if it does, then default config will be used
@@ -99,13 +111,22 @@ fn get_config() -> CliConfig {
 
 fn list_todo(
     path: &std::path::Path,
-    re: &Regex,
-    todo_items: &mut std::collections::HashMap<String, TodoItem>,
+    re: &Arc<Regex>,
+    todo_items: &Arc<Mutex<TodoList>>,
 ) -> Result<()> {
     let content = std::fs::read_to_string(&path)
         .with_context(|| format!("could not read file `{}`", &path.display()))?;
 
-    // TODO: multiline support
+    let content_rewritten_buffer = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path.with_extension("tmp"))
+        .with_context(|| format!("could not open file `{}`", &path.display()))?;
+
+    let mut outbuf = BufWriter::new(Box::new(content_rewritten_buffer));
+
+    // TODO(3): multiline support
     for (i, line) in content.lines().enumerate() {
         match re.captures(line) {
             Some(caps) => {
@@ -119,28 +140,92 @@ fn list_todo(
                 //     path.display(),
                 //     i + 1,
                 // )?;
-                let current_idx = todo_items.len();
-                todo_items.insert(
-                    format!("!{}", current_idx),
-                    TodoItem {
-                        title: title.to_string(),
-                        category: category.to_string(),
-                        path: path.to_path_buf(),
-                        line: i + 1,
-                    },
-                );
+
+                match todo_items.lock() {
+                    Ok(mut todo_items) => {
+                        let id = match caps.name("id") {
+                            Some(id) => {
+                                writeln!(
+                                    outbuf,
+                                    "{}",
+                                    // as is
+                                    line
+                                )?;
+
+                                id.as_str().to_string()
+                            }
+                            None => {
+                                let current_idx = todo_items.items.len();
+                                let id = format!("{}", current_idx);
+
+                                writeln!(
+                                    outbuf,
+                                    "{}",
+                                    re.replace(
+                                        line,
+                                        format!("$before// $category({}): $title", id)
+                                    )
+                                )?;
+
+                                id
+                            }
+                        };
+
+                        todo_items.items.insert(
+                            format!("{}", id),
+                            TodoItem {
+                                title: title.to_string(),
+                                category: category.to_string(),
+                                path: path.to_path_buf(),
+                                line: i + 1,
+                            },
+                        );
+                    }
+                    Err(e) => {
+                        return Err(anyhow::anyhow!("could not lock todo_items: {}", e).into());
+                    }
+                }
             }
-            None => {}
+            None => match writeln!(outbuf, "{}", line) {
+                Ok(_) => {}
+                Err(e) => {
+                    return Err(anyhow::anyhow!("could not write to temp file: {}", e).into());
+                }
+            },
         }
     }
 
+    // overwrite the original file with the rewritten content
+    std::fs::rename(path.with_extension("tmp"), path).with_context(|| {
+        format!(
+            "could not rename file `{}` to `{}`",
+            &path.with_extension("tmp").display(),
+            &path.display()
+        )
+    })?;
+
     Ok(())
 }
+
+fn create_regex(patterns: Vec<&str>) -> Result<Regex> {
+    Regex::new(&format!(
+        // TODO(5): escape even number of other quotation marks
+        r#"^(?<before>[^"]*("[^"]*"[^"]*)*)//\s*(?<category>{})(\((?<id>\d+)\))?:\s*(?<title>.*)"#,
+        patterns.join("|")
+    ))
+    .with_context(|| {
+        format!(
+            "could not create regex from pattern `{}`",
+            patterns.join("|")
+        )
+    })
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let cfg = get_config();
 
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
 
     match args.command {
         Commands::Init => {
@@ -183,11 +268,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     );
                     let patterns = pattern.split(',').collect::<Vec<_>>();
 
-                    let re = Regex::new(&format!(
-                        r"[^/]*(?<todo>//\s*{}:\s*(?<title>.*))",
-                        format!("(?<category>{})", patterns.join("|"))
-                    ))
-                    .unwrap();
+                    let re = Arc::new(create_regex(patterns).unwrap());
 
                     let paths = if let Some(path) = path {
                         vec![path]
@@ -210,10 +291,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                                 .write(true)
                                 .create(true)
                                 .truncate(true)
-                                .open(
-                                    // open a .tmp file first
-                                    path.with_extension("tmp"),
-                                )
+                                .open(path)
                                 .with_context(|| {
                                     format!("could not open file `{}`", &path.display())
                                 })?;
@@ -223,34 +301,104 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         None => std::io::BufWriter::new(Box::new(std::io::stdout())),
                     };
 
-                    let mut todo_items: std::collections::HashMap<String, TodoItem> =
-                        std::collections::HashMap::new();
+                    let is_stdout = out.is_none();
+
+                    let todo_items = Arc::new(Mutex::new(TodoList {
+                        items: std::collections::HashMap::new(),
+                    }));
+
+                    let mut handles = vec![];
 
                     for path in paths {
                         for entry in glob::glob(&path.to_string_lossy())? {
                             match entry {
                                 Ok(path) => {
-                                    list_todo(&path, &re, &mut todo_items)?;
+                                    let todo_items = Arc::clone(&todo_items);
+                                    let re = Arc::clone(&re);
+                                    debug!("processing file: {}", path.display());
+                                    handles.push(thread::spawn(move || {
+                                        list_todo(&path, &re, &todo_items)
+                                    }));
                                 }
                                 Err(e) => eprintln!("error: {}", e),
                             }
                         }
                     }
 
-                    for (_, item) in todo_items.iter() {
+                    for handle in handles {
+                        handle.join().unwrap()?;
+                    }
+
+                    for (id, item) in todo_items.lock().unwrap().items.iter() {
                         writeln!(
                             outbuf,
-                            "- [ ] {}: {} ({}:{})",
+                            "- [ ] {}({}): {} {}({}{}{})",
                             item.category,
+                            id,
                             item.title.trim(),
+                            if is_stdout { "" } else { "[link]" },
                             item.path.display(),
+                            if is_stdout { ":" } else { "#L" },
                             item.line,
                         )?;
                     }
+
+                    // if .mrdm directory does not exist, create it
+                    std::fs::create_dir(".mrdm").ok();
+
+                    // output the todo items to json
+                    let data_out = std::fs::OpenOptions::new()
+                        .write(true)
+                        .create(true)
+                        .truncate(true)
+                        .open(OUT_PATH)
+                        .with_context(|| format!("could not open file `{}`", &OUT_PATH))?;
+
+                    let mut data_writer = BufWriter::new(data_out);
+
+                    match todo_items.lock() {
+                        Ok(todo_items) => {
+                            serde_json::to_writer_pretty(&mut data_writer, &*todo_items)
+                                .with_context(|| {
+                                    format!("could not write to file `{}`", &OUT_PATH)
+                                })?;
+                        }
+                        Err(e) => {
+                            return Err(anyhow::anyhow!("could not lock todo_items: {}", e).into());
+                        }
+                    };
                 }
             }
         }
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_regex() {
+        let re = create_regex(vec!["TODO", "FIXME"]).unwrap();
+
+        let caps = re.captures("// TODO(6): test").unwrap();
+        assert_eq!(caps.name("category").unwrap().as_str(), "TODO");
+        assert_eq!(caps.name("title").unwrap().as_str(), "test");
+
+        let caps = re.captures("// FIXME(2): test").unwrap();
+        assert_eq!(caps.name("category").unwrap().as_str(), "FIXME");
+        assert_eq!(caps.name("id").unwrap().as_str(), "2");
+        assert_eq!(caps.name("title").unwrap().as_str(), "test");
+
+        let caps = re
+            .captures(
+                r#"
+            testing("// TODO: test");"#,
+            )
+            .is_none();
+
+        assert_eq!(caps, true);
+    }
 }
